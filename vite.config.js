993d804209d43f0ceb0bf6830d96192d829f84cd
freddy3rd <clifford.dle.arnejo@gmail.com>
2026-03-19 13:29:35 +0800

    fix contact route

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://aluminum-trade.vercel.app/", // Your backend URL
+        target: "/contact", // Your backend URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
